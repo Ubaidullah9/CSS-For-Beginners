a7e0608e1a1f7514e9756dac1cc40386dfdb9851
Ubaid Ullah <ubaidullah.uoh@gmail.com>
2023-02-25 00:02:32 +0500

    CSS Biginers

---
+++ CSSgym26/script.js
@@ -0,0 +1,9 @@
+function myfunction(){
+    alert('Thank You for Login');
+}
+function amyfunction(){
+    confirm('Sorry i will be add login page under two to three days');
+}
+function bmyfunction(){
+    confirm('you can call me  03493673578');
+}
